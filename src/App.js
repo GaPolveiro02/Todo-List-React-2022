6861284bac09e750eb8ae1d7019ce1ecc1f8f1e7
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-06-07 16:07:43 -0300

    Created todo,todoList and function handleAddTodo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      name: todo
+    }
+
+    setTodoList(prevState => [...prevState, newTodo])
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <input 
+        onChange={e => setTodo(e.target.value)}
+        type="text"
+      ></input>
+      <button onClick={handleAddTodo}>Add</button>
+
+      {todoList.map(todo => <div>{todo.name}</div>)}
     </div>
   );
 }
-
-export default App;

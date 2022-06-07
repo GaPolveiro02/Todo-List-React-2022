3543b42446c6093692982cbb34cc298942bd51b8
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-06-07 16:29:37 -0300

    Created Card actions

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,19 @@
 export function Card(props) {
     return( 
-        <div className="Card">
-            <p>{props.name}</p>
+        <div className="card">
+            <div className="card-content">
+                <p>{props.name}</p>
+            </div>
+            <div className="actions">
+                <a>
+                    <img></img>
+                    Marcar como feita
+                </a>
+                <a>
+                    <img></img>
+                    Excluir
+                </a>
+            </div>
         </div>
     )
 }

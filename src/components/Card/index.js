958225e9f55a4aa08b1ee557b28493abd5bde2a3
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-06-07 18:21:52 -0300

    Added function to change Card className to checked

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,21 @@
 export function Card(props) {
+    
+    function check() {
+        const card = document.querySelector(".card")
+
+        card.classList = "card check"
+    }
+
     return( 
         <div className="card">
             <div className="card-content">
                 <p>{props.name}</p>
             </div>
             <div className="actions">
-                <a>
-                    <img></img>
+                <a href="#" className="check" onClick={check}>
                     Marcar como feita
                 </a>
-                <a>
-                    <img></img>
+                <a href="#" className="delete">
                     Excluir
                 </a>
             </div>

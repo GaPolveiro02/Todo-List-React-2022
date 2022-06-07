886e89ae8195de639d268b0bb8877e162a035039
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-06-07 20:14:55 -0300

    Added condition for an empty input value not to be added in todoList

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import { useState } from "react";
-import { Card } from "./components/Card"
+import { Card } from "./components/Card";
 
 export function App() {
   const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
+  const input = document.querySelector(".input")
+
+  const clearInput = () => {
+    input.value = ''
+  }
 
   const handleAddTodo = () => {
-    const newTodo = {
-      name: todo
+    if (input.value == '') {
+      alert("Digite um todo")
+    } else {
+      const newTodo = {
+        name: todo
+      }
+  
+      setTodoList(prevState => [...prevState, newTodo])
+      clearInput()
+      setTodo('')
     }
-
-    setTodoList(prevState => [...prevState, newTodo])
   }
   
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Todo List</h1>
       <input 
         onChange={e => setTodo(e.target.value)}
         type="text"
+        className="input"
       ></input>
       <button onClick={handleAddTodo}>Add</button>
 
-      {todoList.map(todo => <Card name={todo.name}/>)}
+      {todoList.map(todo => <Card name={todo.name} />)}
     </div>
   );
 }
